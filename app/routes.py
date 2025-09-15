888f92e01c1cf744e1be82a2d152cd0676865ddb
tiagomonge <tiagomonge@gmail.com>
2025-09-15 13:27:49 +0100

    routes.py
    Adiciona as rotas para as páginas login e register

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,17 @@ from flask import render_template
 def index():
     user = {'username': 'David S'}
     return render_template('index.html', title='Home', user=user)
+
+#alteração Tiago Santos
+@app.route('/login')
+def login ()
+    user = {'username': 'David S'}
+    return render_template('login.html', title='Login', user=user)
+    
+@app.route('/register')
+def register ()
+
+    return render_template('register.html', title='Registo', user=user)
+    
+#alteração Tiago Santos
+
